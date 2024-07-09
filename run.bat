56d99658db15c93bfdeedc5bf6a7c786da372f98
Fran Moreno <fran.moreno.se@gmail.com>
2024-07-09 07:52:36 +0200

    [FEAT.v1] Check for requirements.txt

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 REM check if venv exists, create if not
 IF NOT EXIST "venv" (
     python -m venv venv
+    call venv\Scripts\activate
+    pip install -r requirements.txt
+) ELSE (
+    call venv\Scripts\activate
 )
-
-REM activate venv
-call venv\Scripts\activate
 
 REM Open default browser with specified URL
 start "" http:127.0.0.1:5000
